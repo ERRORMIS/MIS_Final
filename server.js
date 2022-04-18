b1b0bb5b6b4b3aa27f2c47db08b2e1891cc0136a
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-04-18 19:33:52 +0530

    send email to reset part 1

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/auth',require('./routes/auth'));
+app.use('/api/private',require('./routes/private'));
+
 
 //Error handler (Should be last plase of middleware )
 app.use(errorHandler);
