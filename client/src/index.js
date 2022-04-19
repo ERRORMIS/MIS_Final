ba5454c7aaa3809e4b1126b6d804af8fb7a55450
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-04-19 08:42:44 +0530

    client/ frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+// import {} from "@fortawesome/free-solid-svg-icons";
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
 
 reportWebVitals();
