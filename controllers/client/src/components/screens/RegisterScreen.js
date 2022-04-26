17dbe5d6368119be547c1800be145da799342085
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-04-26 13:52:06 +0530

    commit before nodemodules

---
+++ controllers/client/src/components/screens/RegisterScreen.js
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./RegisterScreen.css";
+import NavBarLogin from './NavbarLogin'
+
+
+const RegisterScreen = ({ history }) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const registerHandler = async (e) => {
+    e.preventDefault();
+
+    const config = {
+      header: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    if (password !== confirmpassword) {
+      setPassword("");
+      setConfirmPassword("");
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+      return setError("Passwords do not match");
+    }
+
+    try {
+      const { data } = await axios.post(
+        "/api/auth/register",
+        {
+          username,
+          email,
+          password,
+        },
+        config
+      );
+
+      localStorage.setItem("authToken", data.token);
+
+      history.push("/");
+    } catch (error) {
+      setError(error.response.data.error);
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
+
+  return (
+    <div> <NavBarLogin/>
+    <div className="register-screen">
+      <form onSubmit={registerHandler} className="register-screen__form">
+        <h3 className="register-screen__title">Register</h3>
+        {error && <span className="error-message">{error}</span>}
+        <div className="form-group">
+          <label htmlFor="name">Username:</label>
+          <input
+            type="text"
+            required
+            id="name"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            required
+            id="email"
+            placeholder="Email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            required
+            id="password"
+            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,16}$"
+            autoComplete="true"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirmpassword">Confirm Password:</label>
+          <input
+            type="password"
+            required
+            id="confirmpassword"
+            autoComplete="true"
+            placeholder="Confirm password"
+          
+            value={confirmpassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Register
+        </button>
+
+        <span className="register-screen__subtext">
+          Already have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+    </div>
+  );
+};
+
+export default RegisterScreen;
---
+++ controllers/config/db.js
@@ -0,0 +1,13 @@
+const mongoose =require('mongoose');
+
+const connectDB =async() =>{
+    await mongoose.connect(process.env.MONGO_URI,{
+      
+        useUnifiedTopology:true,
+      
+    });
+
+    console.log("MongoDB connected");
+};
+
+module.exports =connectDB;
